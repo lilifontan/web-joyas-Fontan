da8b8ad146f5bd60cd09062f11e336cbd896e1a7
lilifontan <lilianafontan@gmail.com>
2022-07-18 20:06:36 -0300

    web-joyas-Fontan Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+return (
+    <>
+<header>
+
+  <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="./pages/sobreMi.html">Sobre mí</a></li>
+    <li><a href="./pages/novedades.html">Novedades</a></li>
+    <li><a href="./pages/contacto.html">Contacto</a></li>
+    <li><a href="./pages/carrito.html">Carrito de compras</a></li>
+  </ul>
+
+</header>
+</>
+)
+}
+
+export default Navbar
