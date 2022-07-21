f10403ee628d3be7843f7f58d9e151274fed183a
lilifontan <lilianafontan@gmail.com>
2022-07-20 23:46:23 -0300

    web-joyas-Fontan Crea tu landing 3

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -13,14 +13,14 @@ const CardsContainer = () => {
 
         <div class = "card">
         <div class="cartImg scale"> <img src="../src/images/Earrings_resized.jpeg" /> </div>
-        <div class="cartTitle"><h2>Ethereal</h2></div>
+        <div class="cartTitle"><h2>Avar</h2></div>
         <div class="cartPrice"><h2>$15900</h2></div>
         <button data-id="${id}" class="buttonProd"> Agregar al Carrito </button>
         </div>  
 
         <div class = "card">
         <div class="cartImg scale"> <img src="../src/images/Necklace_resized.jpeg" /> </div>
-        <div class="cartTitle"><h2>Ethereal</h2></div>
+        <div class="cartTitle"><h2>Cyon</h2></div>
         <div class="cartPrice"><h2>$16500</h2></div>
         <button data-id="${id}" class="buttonProd"> Agregar al Carrito </button>
         </div>  
