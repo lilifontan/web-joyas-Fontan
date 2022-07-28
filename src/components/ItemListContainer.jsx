e1bc69992636daa397f7acf2b3e32ca89b6cbe98
lilifontan <lilianafontan@gmail.com>
2022-07-28 20:24:55 -0300

    web-joyas-Fontan-Contador 4

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,45 +1,58 @@
 import React, { useEffect, useState } from 'react'
-import ItemList from "./ItemList"
+import ItemList from './ItemList'
 
-    const ItemListContainer = () => {
-        const [listaP,setListaP] =useState([])
-    
-        useEffect(()=>{
-    
-            setListaP([
-                {
-                    id:1,
-                    img: "../src/images/Ring_resized.jpg",
-                    title: "Ethereal",
-                    price: 12500,
-                    stock:5,
-                    initial:0
-                },
-                {
-                    id:2,
-                    img: "../src/images/Earrings_resized.jpeg",
-                    title: "Avar",
-                    price: 15900,
-                    stock:2,
-                    initial:0
-                },
-                {
-                    id:3,
-                    img: "../src/images/Necklace_resized.jpeg",
-                    title: "Cyon",
-                    price: 16500,
-                    stock:8,
-                    initial:5
-                }
+export default function ItemListContainer() {
 
-        ])
-        },[])
-    
-      return (
-        <div id="ID_cardContainer" className="cardContainer" flex >
-        <ItemList lista={listaP} />
-        </div> 
-      )
+const [listaProd, setlistaProd] = useState([])
+
+useEffect(() => {
+let lista = [
+    {
+        id:1,
+        img: "../src/images/Ring_resized.jpg",
+        title: "Ethereal",
+        price: 12500,
+        stock:5,
+        initial:0
+    },
+    {
+        id:2,
+        img: "../src/images/Earrings_resized.jpeg",
+        title: "Avar",
+        price: 15900,
+        stock:2,
+        initial:0
+    },
+    {
+        id:3,
+        img: "../src/images/Necklace_resized.jpeg",
+        title: "Cyon",
+        price: 16500,
+        stock:8,
+        initial:5
     }
-    
-    export default ItemListContainer
+
+] 
+
+
+
+const promesaProductos = new Promise ((res,rej) => {
+
+    setTimeout(() => {
+        res(lista)
+    },2000);
+});
+
+promesaProductos.then ((respuesta)=>{
+    setlistaProd(respuesta)
+})
+},[]);
+
+console.log ("lista prod "+ listaProd)
+
+  return (
+    <div id="ID_cardContainer" className="cardContainer" flex >
+    <ItemList lista = {listaProd}/>
+    </div> 
+  )
+}
---
+++ src/components/ItemListContainer2.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import ItemList from "./ItemList"
+
+    const ItemListContainer2 = () => {
+        const [listaP,setListaP] =useState([])
+    
+        useEffect(()=>{
+    
+            setListaP([
+                {
+                    id:1,
+                    img: "../src/images/Ring_resized.jpg",
+                    title: "Ethereal",
+                    price: 12500,
+                    stock:5,
+                    initial:0
+                },
+                {
+                    id:2,
+                    img: "../src/images/Earrings_resized.jpeg",
+                    title: "Avar",
+                    price: 15900,
+                    stock:2,
+                    initial:0
+                },
+                {
+                    id:3,
+                    img: "../src/images/Necklace_resized.jpeg",
+                    title: "Cyon",
+                    price: 16500,
+                    stock:8,
+                    initial:5
+                }
+
+        ])
+        },[])
+    
+      return (
+        <div id="ID_cardContainer" className="cardContainer" flex >
+        <ItemList lista={listaP} />
+        </div> 
+      )
+    }
+    
+    export default ItemListContainer2
