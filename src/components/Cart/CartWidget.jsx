fe5f5bb37dccbb929477d32b20aad6805d1a26c3
lilifontan <lilianafontan@gmail.com>
2022-09-04 23:06:30 -0300

    Entrega Final React 1

--- src/components/Cart/CartWidget.jsx
+++ src/components/Cart/CartWidget.jsx
@@ -1,24 +1,44 @@
-import React , {useContext, useEffect} from 'react'
+import React , {useContext, useEffect, useState} from 'react'
 import { cartContext } from '../Context/CartContextComponent'
 
 export default function CartWidget() {
 const {cart, totalCount} = useContext (cartContext)
+const [aux, setAux] = useState (false)
 
+useEffect (() => { 
+  if (cart.length != 0) {setAux (true)
+}
+  else {setAux (false)}
 
-useEffect (()=>{
-   
-  }, [cart])
+  }, [cart], aux, setAux)
 
   return (
-    <div>
-    <img
-    src="./src/images/carrito-lila-removebg-preview.png"
-    alt="Logo de la marca"
-    width="50px"
-  /> 
+    <>  {  aux ? (
+        <div>
+        <img
+          src="./src/images/carrito-lila-removebg-preview.png"
+          alt="Logo de la marca"
+          width="50px"
+        /> 
 
- <div className = 'total'> {totalCount} </div>
-  
-    </div>
+      <div className = 'total'> {totalCount} </div>
+        
+          </div>
+        ) : (
+          <div>
+          <img
+            src="./src/images/carrito-lila-removebg-preview.png"
+            alt="Logo de la marca"
+            width="50px"
+          /> 
+          
+            </div>
+
+
+
+        )
+      
+      }
+    </>
   )
 }
